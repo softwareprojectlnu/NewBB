e4d473ef99063cd78aac8fc61d9ca15930b06dcf
software project Group 7 <38452836+softwareprojectlnu@users.noreply.github.com>
2018-04-23 18:54:52 +0200

    The new structure of the project

---
+++ src/app/shared/components/sign/sign.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../services/auth.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-sign',
+  templateUrl: './sign.component.html',
+  styleUrls: ['./sign.component.css']
+})
+export class SignComponent implements OnInit {
+  form: FormGroup;
+
+  constructor(private authService: AuthService, private fb: FormBuilder, private router: Router) {
+    this.form = this.fb.group({
+      'email': ['', [Validators.required, Validators.email]],
+      'password': ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  signin() {
+    if (!this.form.valid) {
+      Object.keys(this.form.controls).forEach(field => {
+        const control = this.form.get(field);
+        control.markAsTouched({ onlySelf: true });
+      });
+      return;
+    }
+    this.authService.loginWithEmail(this.form.controls.email.value, this.form.controls.password.value)
+      .then(() => this.router.navigate(['/home']));
+
+
+  }
+
+}
